9e26a10fd646de93cb3643a256cf61f11c96abcf
baconkak <natwarit@gmail.com>
2024-11-10 17:28:04 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+function showDetails(projectName) {
+    alert("coming soon!");
+}
+
+function myface() {
+    var image = document.getElementById("image");
+    if (image.style.display === "none" || image.style.display === "") {
+        image.style.display = "block"; // Show the image
+    } else {
+        image.style.display = "none"; // Hide the image
+    }
+}
+
+
+function helloWorld() {
+    alert("Hello, World!");
+}
